ea9d1f0b34766b34472fb779bb3f84c88b52a3b3
NelsonSharma <mail.nelsonsharma@gmail.com>
2024-11-07 20:42:38 +0800

    upversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name =                      'sharefly',
-    version =                   '0.0.3',
+    version =                   '0.0.4',
     url =                       'https://github.com/NelsonSharma/sharefly',
     author =                    'Nelson.S',
     author_email =              'mail.nelsonsharma@gmail.com',
